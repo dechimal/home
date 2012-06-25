cba0f42e1e0d1ef8ce5251f483dacb20fb4d85b8
Koichi Oyama <koyama@aiming-inc.com>
2012-06-25 17:15:03 +0900

    いらんファイル削除

--- .gm_scripts/twitter_filter/twitter_filter.user.js
+++
@@ -1,9 +0,0 @@
-// ==UserScript==
-// @name           twitter filter
-// @namespace      http://twitter.com/
-// @include        http://twitter.com/
-// ==/UserScript==
-
-HTMLElement.__proto__ = function() {
-    
-}
